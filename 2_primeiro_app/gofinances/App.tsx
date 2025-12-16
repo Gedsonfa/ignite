c7ab1e08a0396c50d793a24ab9582142b63b22a4
Gedsonfa <gedsonfda@gmail.com>
2025-12-16 14:39:28 -0300

    first commit

---
+++ 2_primeiro_app/gofinances/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from "./src/global/styles/theme";
+
+import { Dashboard } from "./src/screens/Dashboard";
+
+export default function App(){
+  const [fontsLoaded] = useFonts({
+     
+  });
+  return(
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
+  )
+}
