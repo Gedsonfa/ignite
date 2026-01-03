b027cfe34afa90026dd7c8b368081f46f1616b4c
Gedsonfa <gedsonfda@gmail.com>
2026-01-03 16:01:24 -0300

    fix color

--- 2_primeiro_app/gofinances/src/components/Form/Input/styles.ts
+++ 2_primeiro_app/gofinances/src/components/Form/Input/styles.ts
@@ -9,7 +9,8 @@ export const Container = styled(TextInput)`
     font-family: ${({theme})=>theme.fonts.regular};
     font-size: ${RFValue(14)}px;
 
-    background-color: ${({theme}) => theme.colors.text_dark};
+    color: ${({theme}) => theme.colors.text_dark};
+    background-color: ${({theme}) => theme.colors.shape};
     border-radius: 5px;
 
     margin-bottom: 8px;
