6899e998730bf68c8a6d078cfa926029b81e39d6
Gedsonfa <gedsonfda@gmail.com>
2026-01-04 09:58:05 -0300

    #F054 04. Criando o Componente para selecionar o tipo da transação

--- 2_primeiro_app/gofinances/src/screens/Register/index.tsx
+++ 2_primeiro_app/gofinances/src/screens/Register/index.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
 import { Container,
     Header,
     Title,
     Form,
-    Fields
+    Fields,
+    TransactionTypes
  } from './styles';
+import theme from "../../global/@types/theme";
 
 export function Register(){
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
     return(
         <Container>
             <Header>
@@ -19,8 +27,29 @@ export function Register(){
 
             <Form>
                 <Fields>
-                    <Input placeholder="Nome"/>
-                <Input placeholder="Preço"/>
+                    <Input 
+                        placeholder="Nome"
+                        placeholderTextColor={theme.colors.text}
+                    />
+                    <Input 
+                        placeholder="Preço"
+                        placeholderTextColor={theme.colors.text}
+                    />
+                <TransactionTypes>
+                    <TransactionTypeButton
+                    type="up"
+                    title="Income"
+                    onPress={()=>handleTransactionsTypeSelect('up')}
+                    isActive={transactionType === 'up'}
+                    />
+                    <TransactionTypeButton
+                        type="down"
+                        title="Outcome"
+                        onPress={()=>handleTransactionsTypeSelect('down')}
+                        isActive={transactionType === 'down'}    
+
+                    />
+                </TransactionTypes>
                 </Fields>
                 <Button title="Enviar"/>
             </Form>
